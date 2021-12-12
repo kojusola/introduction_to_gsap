c3c6b83024b0aacb67864e7afa9ba6a60a689299
Fafemi Adeola <adeola5678@gmail.com>
2021-12-12 09:36:20 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useRef, useEffect } from "react";
+import { gsap, Power3 } from "gsap";
 
 function App() {
+  let logoItem = useRef(null);
+  let textItem = useRef(null);
+  useEffect(() => {
+    gsap.to(logoItem, 0.8, {
+      opacity: 1,
+      y: -20,
+      ease: Power3.easeOut(),
+    });
+    gsap.to(textItem, 0.8, {
+      opacity: 1,
+      y: -20,
+      ease: Power3.easeOut(),
+      delay: 0.2,
+    });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img
+          ref={(el) => (logoItem = el)}
+          src={logo}
+          className="App-logo"
+          alt="logo"
+        />
+        <p className="App-text" ref={(el) => (textItem = el)}>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
