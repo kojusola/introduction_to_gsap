5dbc0482b26501b5b610414dfac992f38ca5991f
Fafemi Adeola <adeola5678@gmail.com>
2021-12-27 18:15:56 +0100

    stagger and onClick action

--- src/App.js
+++ src/App.js
@@ -1,28 +1,81 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { gsap, Power3 } from "gsap";
 
 function App() {
-  let logoItem = useRef(null);
-  let textItem = useRef(null);
-  useEffect(() => {
-    gsap.to(logoItem, 0.8, {
-      opacity: 1,
-      y: -20,
-      ease: Power3.easeOut(),
+  // let logoItem = useRef(null);
+  // let textItem = useRef(null);
+  const [circleState, setCircleState] = useState(false);
+  let app = useRef(null);
+  let circle = useRef(null);
+  let circleRed = useRef(null);
+  let circleBlue = useRef(null);
+  const handleClick = () => {
+    gsap.to(circleRed, 0.8, {
+      width: 200,
+      height: 200,
+      ease: Power3.easeInOut,
     });
-    gsap.to(textItem, 0.8, {
-      opacity: 1,
-      y: -20,
-      ease: Power3.easeOut(),
-      delay: 0.2,
+    setCircleState(true);
+  };
+
+  const handleClickRevert = () => {
+    gsap.to(circleRed, 0.8, {
+      width: 75,
+      height: 75,
+      ease: Power3.easeInOut,
+    });
+    setCircleState(false);
+  };
+  // useEffect(() => {
+  //   gsap.to(logoItem, 0.8, {
+  //     opacity: 1,
+  //     y: -20,
+  //     ease: Power3.easeOut(),
+  //   });
+  //   gsap.to(textItem, 0.8, {
+  //     opacity: 1,
+  //     y: -20,
+  //     ease: Power3.easeOut(),
+  //     delay: 0.2,
+  //   });
+  // }, []);
+  useEffect(() => {
+    gsap.to(app, 0, { css: { visibility: "visible" } });
+    // gsap.from(circle, 0.8, {
+    //   opacity: 0,
+    //   x: 40,
+    //   ease: Power3.easeOut(),
+    // });
+    // gsap.from(circleRed, 0.8, {
+    //   opacity: 0,
+    //   x: 40,
+    //   ease: Power3.easeOut(),
+    //   delay: 0.2,
+    // });
+    // gsap.from(circleBlue, 0.8, {
+    //   opacity: 0,
+    //   x: 40,
+    //   ease: Power3.easeOut(),
+    //   delay: 0.4,
+    // });
+    gsap.from([circle, circleRed, circleBlue], 0.8, {
+      opacity: 0,
+      ease: Power3.easeOut,
+      x: 40,
+      stagger: 0.2,
     });
   }, []);
   return (
-    <div className="App">
+    <div
+      ref={(el) => {
+        app = el;
+      }}
+      className="App"
+    >
       <header className="App-header">
-        <img
+        {/* <img
           ref={(el) => (logoItem = el)}
           src={logo}
           className="App-logo"
@@ -38,7 +91,16 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <div className="circle-container">
+          <div ref={(el) => (circle = el)} className="circle"></div>
+          <div
+            ref={(el) => (circleRed = el)}
+            onClick={circleState !== true ? handleClick : handleClickRevert}
+            className="circle red"
+          ></div>
+          <div ref={(el) => (circleBlue = el)} className="circle blue"></div>
+        </div>
       </header>
     </div>
   );
